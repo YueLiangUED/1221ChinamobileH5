de97cd166cdb614722273c0fbba737f87858b2e6
ChengYien <cye_orange@163.com>
2017-12-22 10:32:56 +0800

    a

--- js/index.js
+++ js/index.js
@@ -70,7 +70,33 @@ $(function () {
         }
     };
 
+    // 背景音乐
+    ;(function () {
+        var audio = document.getElementById("myAudio");
+        $('.audio-control').on('touchend', function () {
+            if (audio.paused) {
+                $(this).addClass('audioRotate');
+                audio.play();
+            } else {
+                $(this).removeClass('audioRotate');
+                audio.pause()
+            }
+        });
+    })();
 
+    // 滑动
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: false,
+        mousewheelControl : true,
+        onInit: function(swiper){
+            swiperAnimateCache(swiper);
+            swiperAnimate(swiper);
+        }, 
+        onSlideChangeEnd: function(swiper){ 
+          swiperAnimate(swiper);
+        } 
+    });
 
     /**********************************************绑定事件**********************************************/
 
@@ -113,7 +139,28 @@ $(function () {
         'images/img_05_04.png',
         'images/img_05_05.png',
         'images/img_06_01.png',
+        'images/img_07_01.png',
+        'images/img_07_02.png',
+        'images/img_08_01.png',
+        'images/img_08_02.png',
+        'images/img_08_03.png',
+        'images/img_08_04.png',
+        'images/img_08_05.png',
+        'images/img_09_01.png',
+        'images/img_09_02.png',
+        'images/img_09_03.png',
+        'images/img_10_01.png',
+        'images/img_10_02.png',
+        'images/img_10_03.png',
+        'images/img_10_04.png',
+        'images/img_10_05.png',
+        'images/img_10_06.png',
+        'images/img_10_07.png',
+        'images/img_10_08.png',
+        'images/img_11_01.png',
+        'images/img_11_02.png'
     ];
+
     $('.progress-num').uImgLoad(pics,$('.progress-num'),function () {
         $('.page-loading').fadeOut('fast', function () {
             $('.login').fadeIn('fast');
@@ -129,7 +176,7 @@ $(function () {
     
     // 点击获取验证码
     $('.login-form-code-btn').on('touchend', function () {
-        $(this).getCode(70);
+        $(this).getCode(60);
     });
 
     //点击登录
@@ -145,7 +192,11 @@ $(function () {
             return false;
         }
 
-        // 验证成功后
+        // 需要在此验证用户手机号是否为受邀用户，如果非受邀用户提示：登录失败，您不是本次活动受邀用户！
+
+
+
+        // 验证成功后的操作
         $('.login').slideUp();
         $('.first-content .ani').addClass('animated');
         $('.first-content .ani').each(function () {
@@ -154,17 +205,25 @@ $(function () {
         });
     });
 
-    // 滑动
-    var mySwiper = new Swiper ('.swiper-container', {
-        direction: 'vertical',
-        loop: false,
-        mousewheelControl : true,
-        onInit: function(swiper){
-            swiperAnimateCache(swiper);
-            swiperAnimate(swiper);
-        }, 
-        onSlideChangeEnd: function(swiper){ 
-          swiperAnimate(swiper);
-        } 
+    
+    // 点最后一页击确认按钮办理业务
+    $('#enter').on('touchend', function () {
+        var t = $('#handleInput').val();
+        if (t === '') {
+            alert('请输入办理此业务的手机号码！');
+            return false;
+        }
+
+        // 需要验证用户（受邀用户）：
+        //          是否已经赠送他人或为自己办理过体验包，如果办理过提示：您已经办理过业务体验包！
+        //          
+        //          输入的手机号（最后一页）是否已经办理过体验包，如果已经办理过提示：此号码已经办理过体验包！
+        //          
+        // 用户（受邀用户）未赠送过体验包或他人未给自己办理过体验包
+        alert("办理成功！");  
+        window.location.href="http://www.bj.10086.cn/m"     
     });
+    
+
+    
 });
